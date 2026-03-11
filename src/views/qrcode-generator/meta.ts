ff0f28360f22cf902a885289655d2ff710adb9a7
Kazima <48854262+ductongnguyen@users.noreply.github.com>
2026-03-12 00:45:30 +0700

    feat: add qr code generator with custom styling and frame support (#231)

---
+++ src/views/qrcode-generator/meta.ts
@@ -0,0 +1,7 @@
+export default {
+  name: 'QR Code Generator',
+  description: 'Professional QR generator with custom styling and frame support',
+  author: 'ductongnguyen',
+  facebook: 'https://www.facebook.com/it.tongnd/',
+  category: 'tool',
+}
